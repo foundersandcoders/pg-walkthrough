2804134d44f8c8eef3d7291cd035ff03f6e1069e
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-03-19 21:56:19 +0200

    Uses es6 syntax Relates #1

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-var http = require('http');
-var handler = require('./handler.js');
+const http = require('http');
+const handler = require('./handler.js');
 
 const server = http.createServer(handler);
 const port = process.env.PORT || 3000;
