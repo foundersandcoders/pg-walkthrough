c89f5a19adde5db9dd48c743f2c36b10e09104ac
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-03-27 18:20:33 +0300

    Adds instructions to connect to database

--- database/db_connection.js
+++ database/db_connection.js
@@ -15,10 +15,10 @@ const options = {
   port: params.port,
   database: params.pathname.split('/')[1],
   max: process.env.DB_MAX_CONNECTIONS || 2,
+  user: username,
+  password: password
 };
 
-options.user = username;
-options.password = password;
 options.ssl = (options.host !== 'localhost');
 
 module.exports = new Pool(options);
