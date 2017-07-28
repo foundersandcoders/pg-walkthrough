bbd3a58df417854286fcdf5705ee8b09291e82a5
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-07-28 13:52:03 +0100

    added step 5 notes and code relates #15

---
+++ walkthroughSteps/step-5/src/dynamic.js
@@ -0,0 +1,11 @@
+const dbConnection = require('../database/db_connection.js');
+
+const getData = (cb) =>{
+    dbConnection.query(`SELECT * FROM superheroes`
+    , (err, res) => {
+    if (err) cb(err);
+    cb(null, res.rows);
+  });
+};
+
+module.exports = getData;
