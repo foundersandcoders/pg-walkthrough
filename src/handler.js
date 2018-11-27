449dee7b1150624699854a4d04abe07123ca9a29
matt lubel <matt_lubel@hotmail.com>
2018-11-27 14:57:34 +0200

    add start files to src folder in root

---
+++ src/handler.js
@@ -0,0 +1,15 @@
+const staticSuperHeroes = require('./static.js');
+
+const handler = (request, response) => {
+  let endpoint = request.url.split('/')[1];
+
+  if(endpoint === 'static'){
+    let staticData = JSON.stringify(staticSuperHeroes);
+    response.writeHead(200,{
+      'content-type': 'application/json'
+    });
+    response.end(staticData);
+  }
+};
+
+module.exports = handler;
